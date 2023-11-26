487f30e009be71e0d3aee4ed331567f50e2c7505
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <119198739+PyAaditya@users.noreply.github.com>
2023-11-26 13:25:35 +0530

    Update chat_control.py

--- TheXSpam/chat_control.py
+++ TheXSpam/chat_control.py
@@ -1,7 +1,3 @@
-# 𝑪𝒐𝒑𝒚𝒓𝒊𝒈𝒉𝒕 𝑩𝒚 𝑨𝒍𝒕𝒓𝒐𝒏
-# 𝑨𝒍𝒍 𝑹𝒊𝒈𝒉𝒕𝒔 𝑹𝒆𝒔𝒆𝒓𝒗𝒆𝒅
-
-
 from config import SUDO_USERS
 from data import GROUP
 
@@ -13,11 +9,11 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command(["join"], ["/", "!", "."]) & filters.user(SUDO_USERS))
 async def join(client: Client, message: Message):
-    alt = message.text.split(" ")
-    if len(alt) == 1:
+    zen = message.text.split(" ")
+    if len(zen) == 1:
         return await message.reply_text("`Need a chat username or chat-id or invite link to join.`")
     try:
-        await client.join_chat(alt[1])
+        await client.join_chat(zen[1])
         await message.reply_text(f"**Joined**")
     except Exception as ex:
         await message.reply_text(f"**ERROR:** \n\n{str(ex)}")
@@ -28,8 +24,8 @@ async def join(client: Client, message: Message):
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["leave", "remove"], [".", "!", "/"]))
 async def leave(xspam: Client, message: Message):
     alt = message.text.split(" ")
-    if len(alt) > 1:
-        if alt[1] in GROUP:
+    if len(zen) > 1:
+        if zen[1] in GROUP:
             return
         try:
            await xspam.leave_chat(alt[1])
